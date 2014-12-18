39aa08527278ad17d5f2f16b2d7b3b95e9027776
matektomek <matomasz@gmail.com>
2014-12-18 16:30:07 +0100

    zmiana działania menu

--- src/com/app/kfe/MainActivity.java
+++ src/com/app/kfe/MainActivity.java
@@ -11,19 +11,22 @@ import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 	
 	private Button draw2_btn;
-	private Button dolacz_btn;
+	//private Button dolacz_btn;
 	private Button setting_btn;
 	private Button score_btn;
 	private Button exit_btn;
@@ -32,11 +35,12 @@ public class MainActivity extends Activity {
 	DatabaseHelper db;
 	boolean silent;
 	SharedPreferences settings;
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menu);
+        setContentView(R.layout.menuv2);
         mpButtonClick = MediaPlayer.create(this, R.raw.button);
         db = new DatabaseHelper(getApplicationContext());
          settings = getApplicationContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
@@ -44,8 +48,9 @@ public class MainActivity extends Activity {
         if(db.isEmpty("hasla")){
         db.createHasla();
         }
-       
+      
         addListenerOnButton();
+        
     }
 
 
@@ -71,56 +76,57 @@ public class MainActivity extends Activity {
     public void addListenerOnButton() {
     	 
     	draw2_btn = (Button) findViewById(R.id.draw2_btn);
-    	dolacz_btn = (Button) findViewById(R.id.dolacz_btn);
+    //	dolacz_btn = (Button) findViewById(R.id.dolacz_btn);
     	setting_btn = (Button) findViewById(R.id.setting_btn);
     	score_btn = (Button) findViewById(R.id.score_btn);
     	exit_btn = (Button) findViewById(R.id.exit_btn);
     	 final Animation animAlpha = AnimationUtils.loadAnimation(this, R.layout.anim_alpha);
-    	draw2_btn.setOnClickListener(new Button.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				v.startAnimation(animAlpha);
-				   silent = settings.getBoolean("silentMode", true);
-				if((mpButtonClick != null) && silent){
-					mpButtonClick.start();
-					}
-				Handler handler = new Handler();
-				handler.postDelayed(new Runnable(){
-				    public void run() {
-				    	Intent tablica = new Intent(getApplicationContext(), Tablica.class);
-						startActivity(tablica);
-				        }
-				}, 600);
-				
-			}
-			
-			
+    	  registerForContextMenu(draw2_btn);
+//    	draw2_btn.setOnClickListener(new Button.OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				v.startAnimation(animAlpha);
+//				   silent = settings.getBoolean("silentMode", true);
+//				if((mpButtonClick != null) && silent){
+//					mpButtonClick.start();
+//					}
+//				Handler handler = new Handler();
+//				handler.postDelayed(new Runnable(){
+//				    public void run() {
+//				    	Intent tablica = new Intent(getApplicationContext(), Tablica.class);
+//						startActivity(tablica);
+//				        }
+//				}, 600);
+//				
+//			}
+//			
+//			
+//    	
+//    	});
     	
-    	});
-    	
-    	dolacz_btn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
-				v.startAnimation(animAlpha);
-				silent = settings.getBoolean("silentMode", true);
-				if((mpButtonClick != null) && silent){
-					mpButtonClick.start();
-					}
-				Handler handler = new Handler();
-				handler.postDelayed(new Runnable(){
-				    public void run() {
-				    	Intent dolacz = new Intent(getApplicationContext(), com.app.kfe.wifi.WiFiDirectActivity.class);
-						startActivity(dolacz);
-				        }
-				}, 600);
-			}	
-    	
-    	});
+//    	dolacz_btn.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				
+//				v.startAnimation(animAlpha);
+//				silent = settings.getBoolean("silentMode", true);
+//				if((mpButtonClick != null) && silent){
+//					mpButtonClick.start();
+//					}
+//				Handler handler = new Handler();
+//				handler.postDelayed(new Runnable(){
+//				    public void run() {
+//				    	Intent dolacz = new Intent(getApplicationContext(), com.app.kfe.wifi.WiFiDirectActivity.class);
+//						startActivity(dolacz);
+//				        }
+//				}, 600);
+//			}	
+//    	
+//    	});
     	setting_btn.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -189,4 +195,60 @@ public class MainActivity extends Activity {
     	});
     	
 	}
-}
+    
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+    {
+            super.onCreateContextMenu(menu, v, menuInfo);
+            menu.setHeaderTitle("Select The Action"); 
+            menu.add(0, v.getId(), 0, "Single Player"); 
+            menu.add(0, v.getId(), 0, "Multi Player");
+
+    }
+
+    @Override 
+    public boolean onContextItemSelected(MenuItem item)
+    { 
+
+
+             //   AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+              //  String number;
+      
+                try
+                {
+                       
+       
+        
+                        if(item.getTitle()=="Call")
+                        {
+        
+        
+                        	Intent tablica = new Intent(getApplicationContext(), Tablica.class);
+    						startActivity(tablica);
+        
+        
+        
+
+
+                        } 
+                        else if(item.getTitle()=="Send SMS")
+                        {
+                        	Intent dolacz = new Intent(getApplicationContext(), com.app.kfe.wifi.WiFiDirectActivity.class);
+   						startActivity(dolacz);
+            
+
+                        } 
+                        else
+                        {return false;} 
+                        return true; 
+                }
+                catch(Exception e)
+                {
+                        return true;
+                }
+    } 
+   
+   
+
+} 
+
