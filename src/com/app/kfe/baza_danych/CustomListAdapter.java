34b1148ae982086180a296a686619aa199d2e6f4
matektomek <matomasz@gmail.com>
2015-01-13 16:55:22 +0100

    PRACE NA ROZGRYWKA

--- src/com/app/kfe/baza_danych/CustomListAdapter.java
+++ src/com/app/kfe/baza_danych/CustomListAdapter.java
@@ -14,23 +14,23 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 public class CustomListAdapter extends ArrayAdapter<String>{
-private final Activity context;
-private final List<String> Gracz;
-private final List<Integer> punkty;
-public CustomListAdapter(Activity context, List<String> Gracz,List<Integer> punkty) {
-super(context, R.layout.stat_row_list,Gracz);
-this.context = context;
-this.Gracz = Gracz;
-this.punkty = punkty;
-}
-@Override
-public View getView(int position, View view, ViewGroup parent) {
-LayoutInflater inflater = context.getLayoutInflater();
-View rowView= inflater.inflate(R.layout.stat_row_list, null, true);
-TextView txtGracz = (TextView) rowView.findViewById(R.id.Gracz);
-TextView txtPunkty = (TextView) rowView.findViewById(R.id.punkty);
-txtGracz.setText(Gracz.get(position));
-txtPunkty.setText(Integer.toString(punkty.get(position)));
-return rowView;
-}
+	private final Activity context;
+	private final List<String> Gracz;
+	private final List<Integer> punkty;
+	public CustomListAdapter(Activity context, List<String> Gracz,List<Integer> punkty) {
+		super(context, R.layout.stat_row_list,Gracz);
+		this.context = context;
+		this.Gracz = Gracz;
+		this.punkty = punkty;
+	}
+	@Override
+	public View getView(int position, View view, ViewGroup parent) {
+		LayoutInflater inflater = context.getLayoutInflater();
+		View rowView= inflater.inflate(R.layout.stat_row_list, null, true);
+		TextView txtGracz = (TextView) rowView.findViewById(R.id.Gracz);
+		TextView txtPunkty = (TextView) rowView.findViewById(R.id.punkty);
+		txtGracz.setText(Gracz.get(position));
+		txtPunkty.setText(Integer.toString(punkty.get(position)));
+		return rowView;
+	}
 }
--- src/com/app/kfe/rysowanie/Rozgrywka.java
+++ src/com/app/kfe/rysowanie/Rozgrywka.java
@@ -2,19 +2,37 @@ package com.app.kfe.rysowanie;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import android.content.Context;
+
+import sqlite.helper.DatabaseHelper;
+import sqlite.model.Haslo;
 
 public class Rozgrywka {
 
 	public int runda=1;
 	public int czas;
 	public List<Gracz> lista_graczy = new ArrayList<Gracz>();
+	public List<Integer> listaUzytychHasel = new ArrayList<Integer>();
 	public String haslo;
+	public DatabaseHelper db;
 	
 	
 	public String losuj_haslo()
 	{
+		Random rand = new Random();
+		int i = rand.nextInt();
 		
 		return haslo;
+	}
+	
+	public ArrayList<Haslo> getAllHasla(Context context){
+		if( db == null){
+			db = new DatabaseHelper(context);
+		}
+		
+		return db.getAllHasla();
 	}
 	
 	
